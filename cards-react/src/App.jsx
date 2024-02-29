8948a1c118a13a0ae09a8ee63092ac515a3f8290
Sjedsson <baqirsyed10@gmail.com>
2024-02-29 10:54:01 +0100

    cards

---
+++ cards-react/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import card from './Card.jsx'
+
+function App() {
+
+  return (
+    <Card />
+  )
+}
+
+export default App
---
+++ cards-react/src/Card.jsx
@@ -0,0 +1,11 @@
+function card (){
+    return(
+        <div className="cards">
+            <h1>A Sign of Four</h1>
+            <h2>av Sir Canon Doyle</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, soluta.</p>
+            <div> <a href="#">Add to Cart</a></div>
+        </div>
+    );
+}
+export default card
